84fe43d6606ebb7d4f145170680dca1a8865d109
elsekily <youssefelsekily@outlook.com>
2024-03-29 14:49:59 +0200

    add home card component

---
+++ src/app/components/home-card/home-card.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-home-card',
+  templateUrl: './home-card.component.html',
+  styleUrls: ['./home-card.component.css']
+})
+export class HomeCardComponent {
+  @Input() product: any;
+}
