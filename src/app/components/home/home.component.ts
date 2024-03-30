2150efb278312e1c32eaef716d596b8e98744849
elsekily <youssefelsekily@outlook.com>
2024-03-30 17:25:55 +0200

    add maze to home cards

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,5 +9,6 @@ export class HomeComponent {
 products = [
     { name: 'Color Sort Solver', imageUrl: 'assets/images/colortube.png' ,link : '/color-tube', description: 'Input your game and I show you how to solve it' },
     { name: 'Snake', imageUrl: 'assets/images/snake.png' ,link : '/snake', description: 'Just Play Snake' },
+    { name: 'Maze Explorer and Solver', imageUrl: 'assets/images/maze.png' ,link : '/maze', description: 'Build your maze and I will explorer it' },
   ];
 }
